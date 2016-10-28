32a1ab6996cde5915180bd4bdb076a6bd4383524
Habib M. Khan <habibmkhan92@gmail.com>
2016-10-28 00:07:04 -0700

    Code Ruckus init

---
+++ src/scripts/routes/account.route.js
@@ -0,0 +1,30 @@
+'use strict';
+
+(function () {
+
+    function accountRouter($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider
+            .otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                views: {
+                    'header': {
+                        templateUrl: 'views/shared/_header.html',
+                        controller: 'headerController'
+                    },
+                    'main': {
+                        controller: 'homeController',
+                        templateUrl: 'views/account/home.html'
+                    }
+                },
+                cache: false,
+                title: 'Code Ruckus',
+                loginRequired: true
+            });
+    }
+
+    app.config(['$stateProvider', '$urlRouterProvider', accountRouter]);
+
+})();
