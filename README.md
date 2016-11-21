16ff1226468f0151717b7e5804aa7426554395c9
Habib M. Khan <habibmkhan92@gmail.com>
2016-11-21 03:32:52 -0800

    beta test prototype

--- README.md
+++
@@ -1,5 +0,0 @@
-# HabibMKhan.github.io
-Habib Khan's personal website currently overwritten by a game I'm working on...
-
-# coderuckus
-game that teaches code
--- scripts/routes/account.route.js
+++ scripts/routes/account.route.js
@@ -7,21 +7,100 @@
             .otherwise('/');
 
         $stateProvider
+            .state('battle', {
+              url: '/battle',
+              views: {
+                'header': {
+                  templateUrl: 'views/account/_header.html',
+                  controller: 'headerController'
+                },
+                'main': {
+                  controller: 'battleController',
+                  templateUrl: 'views/account/battle.html'
+                }
+              },
+              title: 'Code Ruckus BATTLE!',
+            })
+            .state('village', {
+              url: '/village',
+              views: {
+                'header': {
+                  templateUrl: 'views/account/_header.html',
+                  controller: 'headerController'
+                },
+                'main': {
+                  controller: 'villageController',
+                  templateUrl: 'views/account/village.html'
+                }
+              },
+              title: 'Code Ruckus Village',
+            })
+            .state('stats', {
+              url: '/stats',
+              views: {
+                'header': {
+                  templateUrl: 'views/account/_header.html',
+                  controller: 'headerController'
+                },
+                'main': {
+                  controller: 'statsController',
+                  templateUrl: 'views/account/stats.html'
+                }
+              },
+              title: 'Character Basic Stats',
+            })
+            .state('edit', {
+              url: '/edit',
+              views: {
+                'header': {
+                  templateUrl: 'views/account/_header.html',
+                  controller: 'headerController'
+                },
+                'main': {
+                  controller: 'editController',
+                  templateUrl: 'views/account/edit.html'
+                }
+              },
+              params: {
+                chartype: null,
+                subclass: null,
+                name: null,
+                hiddenParam: 'YES'
+              },
+              title: 'Character Edit Stats',
+            })
+            .state('char', {
+                url: '/char',
+                views: {
+                    'header': {
+                        templateUrl: 'views/account/_header.html',
+                        controller: 'headerController'
+                    },
+                    'main': {
+                        controller: 'charController',
+                        templateUrl: 'views/account/char.html',
+                        params: { selectedChar: null }
+                    }
+                },
+                cache: false,
+                title: 'CR Choose Char',
+                loginRequired: false
+            })
             .state('home', {
                 url: '/',
                 views: {
                     'header': {
-                        templateUrl: './views/account/header.html',
+                        templateUrl: 'views/account/_header.html',
                         controller: 'headerController'
                     },
                     'main': {
                         controller: 'homeController',
-                        templateUrl: './views/account/home.html'
+                        templateUrl: 'views/account/home.html'
                     }
                 },
                 cache: false,
                 title: 'Code Ruckus',
-                loginRequired: true
+                loginRequired: false
             });
     }
 
